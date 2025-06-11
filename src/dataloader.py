8b738922852bddc135e90a763eb3676e0c1fcccc
syzygy608 <hankwu99@gmail.com>
2025-06-11 15:49:50 +0800

    remove two outstanding company

--- src/dataloader.py
+++ src/dataloader.py
@@ -245,7 +245,7 @@ def preprocess_data(data_dir):
             file_path = os.path.join(data_dir, file)
             df = pd.read_csv(file_path)
             df["Company Name"] = file.split(".")[0]  # 使用檔名作為公司名稱
-            if df["Company Name"] == "PCLN" or "BRK-A":
+            if df["Company Name"] == "PCLN" or df["Company Name"] == "BRK-A":
                 continue  # 跳過 PCLN 和 BRK-A 公司
             all_data.append(df)
     
